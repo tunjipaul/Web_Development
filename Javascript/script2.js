bdd4d1b53f28192a4ffce2e0bf47b1bb88281fb5
TunjiPaul <ogorpaul877@gmail.com>
2025-09-22 15:44:47 +0100

    Javascript notebook

---
+++ Javascript/script2.js
@@ -0,0 +1,13 @@
+function runFirstProgram(){
+    // this is a comment. 
+    // let's display a message in our output area.
+    document.getElementById("first-output").innerHTML = "hello world! this message was created by javascript!";
+    // also show it in the browser console (open developer tools to see)
+    console.log("hello from javascript console!");
+}
+
+function showAlert(){
+    //show a popup alert
+    alert("this is a javascript alert!");
+}
+

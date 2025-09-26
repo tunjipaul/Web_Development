649d8fcdd44f9fef4c2bf8510a7edeabf896f2a3
your name <ogorpaul877@gmail.com>
2025-09-26 17:43:14 +0100

    updated Javascript codebook 3, exercise 1-3

--- Javascript/Javascript_3/javascriptPractice3_E1.js
+++ Javascript/Javascript_3/javascriptPractice3_E1.js
@@ -17,8 +17,95 @@ for (let i = 0; i < shoppingList.length; i++) {
     console.log(`Item ${i + 1}: ${shoppingList[i]}`);
 }
 
-//2. students grade analyzer.
+// 2. students grade analyzer.
 
-let grades = []
 
+grades = [45, 67, 89, 90, 23, 10, 32, 54, 78, 150];
+
+
+function getAverage(grades) {
+
+    let totalGrade = 0;
+    for (let grade of grades) {
+        totalGrade += grade; //this means total = total + grade; i.e. add all the grade in grades to get the total grades
+    }
+
+    let averageGrades = totalGrade / grades.length;
+    console.log(`Average Grades: ${averageGrades}`);
+}
+getAverage(grades);
+
+function getHighest(grades) {
+    let highestGrade = Math.max(...grades);
+    console.log(highestGrade);
+}
+
+getHighest(grades);
+
+function getLowest(grades) {
+    let lowestGrades = Math.min(...grades);
+    console.log(lowestGrades)
+}
+
+getLowest(grades);
+
+function getPassRate(grades){
+    let passing = 0;
+    for (let grade of grades){
+         if (grade >= 50){
+          passing++;
+         }
+    }
+    console.log(`Passed Grades: ${passing}`)
+    let passRate = 0;
+    passRate = (passing/grades.length) * 100
+    console.log(`PassRate: ${passRate}%`);
+}
+
+getPassRate(grades)
+
+MovieRatingSystem
+
+
+movies = [];
+
+ratings = [];
+
+function addMovie(movie, rating){
+
+ movies.push(movie);
+ ratings.push(rating);
+ console.log("Added movies with rating:", movie, ':', rating)
+
+}
+
+addMovie('KotoAye', 9.4);
+addMovie('GOT', 9.9)
+
+
+
+function getAverageRating(){
+    let totalRating = 0;
+    for (rating of ratings){
+        totalRating += rating;
+    }
+    console.log(`TotalRating: ${totalRating}`);
+    let averageRating = (totalRating/ratings.length)
+    console.log(`Average Rating: ${averageRating}`);
+}
+
+function getTopMovie(){
+    let highestRated = Math.max(...ratings);
+    console.log(highestRated);
+}
+
+
+function getSummary(){
+    console.log("Movie Summary:")
+    for (i = 0; i < movies.length; i++){
+        console.log(movies[i], "-", ratings[i]);
+    }
+}
+
+getSummary()
 
